d6cf4e9191b7491379c0a63a2dc3abe27b1218e9
Sergiy Korotunov <korotunovsergey@gmail.com>
2017-05-01 18:41:54 +0300

    Add styling and DB connection

---
+++ ProjectPortfolioRiskManager/ProjectPortfolioRiskManager.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ProjectPortfolioRiskManager.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ViewResult Register()
+        {
+            return View();
+        }
+    }
+}
